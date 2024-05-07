07e308318cace9bf8a87530fa81693d8ef8f6549
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-05-07 16:25:54 +0200

    add css on the top page

--- my-react-app/src/component/home_page/Home.jsx
+++ my-react-app/src/component/home_page/Home.jsx
@@ -2,24 +2,25 @@ import { Link } from "react-router-dom";
 import weather from "/src/assets/weather.png";
 import todolist from "/src/assets/todolist.png";
 import meditation from "/src/assets/meditation.png";
+import "./home.css";
 
 function Home() {
   return (
     <>
-      <div className="containerHome">
-        <div>
+      <div>
+        <div className="weatherPart">
           <Link to="/weather">
             <img src={weather} alt="Weather Icon" />
             <p>Weather</p>
           </Link>
         </div>
-        <div>
+        <div className="todolistPart">
           <Link to="/todolist">
             <img src={todolist} alt="To Do List Icon" />
             <p>To Do List</p>
           </Link>
         </div>
-        <div>
+        <div className="meditationPart">
           <Link to="/meditation">
             <img src={meditation} alt="Meditation Icon" />
             <p>Meditatation</p>
