6b9fd961ea770ae922e912b3251052d257bc3450
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-05-13 15:21:08 +0200

    fixed map inside of meditation.jsx

--- my-react-app/src/component/meditation_page/MeditationTips.jsx
+++ my-react-app/src/component/meditation_page/MeditationTips.jsx
@@ -1,40 +1,53 @@
-import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import meditationTipsData from "../../../data/meditationtips.json";
+import { useState } from "react";
 import PropTypes from "prop-types";
 function MeditationTips() {
-  const [tips, setTips] = useState([]); // State to store the selected tip
-  const { id } = useParams();
+  const [index, setIndex] = useState(null);
 
-  useEffect(() => {
-    // Filter the tip based on the id from the URL
-    const selectedTip = meditationTipsData.Tips.find(
-      (tip) => tip.id === parseInt(id)
-    );
-    setTips(selectedTip ? [selectedTip] : []);
-  }, [id]); // Re-run effect when id changes
+  const tips = [
+    {
+      title: "Why we need meditation?",
+      body: "Meditation improve self-control and sleeping",
+      id: 0,
+    },
+    {
+      title: "What the disadvantage ? ",
+      body: "It might be difficult to find a time and continue as daily routine at first",
+      id: 1,
+    },
+  ];
 
-  console.log(meditationTipsData);
-  console.log(id);
+  function handleClick(id) {
+    setIndex(id);
+  }
 
   return (
     <>
       <h2>Meditation Tips</h2>
-      {/* Render the selected tip */}
-      {tips.map((tip) => (
-        <div key={tip.id}>
-          <Link to={`/meditation/${tip.id}`}>
-            <h3>{tip.title}</h3>
-          </Link>
-        </div>
-      ))}
+      <div>
+        {index !== null ? (
+          <div>
+            <h2>{tips[0].title}</h2>
+            <p>{tips[0].body}</p>
+          </div>
+        ) : (
+          tips.map((tip) => (
+            <button
+              type="button"
+              key={tip.id}
+              onClick={() => handleClick(tip.id)}
+            >
+              {tip.title}
+            </button>
+          ))
+        )}
+      </div>
     </>
   );
 }
 
 MeditationTips.propTypes = {
   map: PropTypes.func.isRequired,
-  tips: PropTypes.string.isRequired,
+  tips: PropTypes.array.isRequired,
 };
 
 export default MeditationTips;
